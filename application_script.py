ca1c3e1c37f37d6945b9c34f20aeb35782df9da0
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-02-21 12:07:00 +0100

    fix: remove scalability for OS

--- application_script.py
+++ application_script.py
@@ -4,6 +4,7 @@ import subprocess
 import platform
 import os
 
+# script to simulate ctr+cmd+f 
 fullScreen = """
 tell application "System Events"
     tell application "Notion" to activate
@@ -19,32 +20,20 @@ end tell
 
 # This function assumes 
 # * Static coordinates for buttons 
-def login(os_name):
-    # Opening application
-    # Dependent on OS
-    # 'posix' for Linux and macOS, 'nt' for Windows
-    if os_name == 'nt':
-        print("You're using Windows.")
-        notion_path = "C:\\Users\\Mitali\\Downloads\\Notion Setup 2.0.47.exe" # notion path change 
-        subprocess.Popen(notion_path)
-    else:
-        print("You're likely using Linux or macOS.")
-        subprocess.run(["open", "/Applications/Notion.app"])
+def login():
+    subprocess.run(["open", "/Applications/Notion.app"])
 
     time.sleep(3)
     pyautogui.moveTo(720, 320)  # Move to on google login button
     pyautogui.click()
 
     time.sleep(3)
-    pyautogui.moveTo(700, 550)  # Move to correct google account  
+    pyautogui.moveTo(700, 550)  # Move to correct google account for login  
     pyautogui.click()   
 
     time.sleep(3)
-    subprocess.run(["osascript", "-e", fullScreen]) # Open full screen
+    subprocess.run(["osascript", "-e", fullScreen]) # Activate full screen
 
-
-def new_page():
-    pyautogui.moveTo(78, 145)  # Move to "new page"-button  
 
 def cleanup():
     time.sleep(1)
@@ -61,14 +50,15 @@ def cleanup():
 
     time.sleep(2)
     subprocess.run(["osascript", "-e", quit]) # Open full screen
+    # Should write a code to delete the files created
+
+def new_page():
+    pyautogui.moveTo(78, 145)  # Move to "new page"-button  
 
 
-
-
-
+# Assumes macOS
 def main():
-    os_name = platform.system()
-    login(os_name)
+    login()
     cleanup()
 
 
