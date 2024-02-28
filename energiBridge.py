9b50da627d9c8f5e8af1f5bcb297ef0900ad3f43
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-02-28 15:27:35 +0100

    fix: remove defaulted option in command

--- energiBridge.py
+++ energiBridge.py
@@ -16,10 +16,10 @@ def open_terminal():
 
 def generate_command(script_function, count, total_count):
     if script_function == 'web':
-        command = f"./energibridge -o {script_function}_{count}_{total_count}.csv --summary -m=0 /Users/piaasbjornsen/Documents/V2024/SSE/SSE_notion/application_script.py"
+        command = f"./energibridge -o {script_function}_{count}_{total_count}.csv --summary /Users/piaasbjornsen/Documents/V2024/SSE/SSE_notion/application_script.py"
         return command
     if script_function == 'desktop':
-        command = f"./energibridge -o {script_function}_{count}_{total_count}.csv --summary -m=0 /Users/piaasbjornsen/Documents/V2024/SSE/SSE_notion/chromewebscript.py"
+        command = f"./energibridge -o {script_function}_{count}_{total_count}.csv --summary /Users/piaasbjornsen/Documents/V2024/SSE/SSE_notion/chromewebscript.py"
         return command
 
 # Script to be run in main
